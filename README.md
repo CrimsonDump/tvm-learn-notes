2a9412070ca929a3b306bfd6ed6b29a435632c01
xiebaijie <xiebaijie.xbj@alibaba-inc.com>
2025-02-14 11:00:33 +0000

    learning paper

--- README.md
+++ README.md
@@ -5,6 +5,33 @@
 
 # relax
 
+## paper notes
+[Relax: Composable Abstractions for End-to-End Dynamic Machine Learningpaper](https://arxiv.org/pdf/2311.02103)
+
+### Language Constructs
+annotation, dataflow block, function call
+### Cross-level abstraction
+
+relax (DAG style) ---call_tir/call_dps_library---> tir/library(DPS style)
+
+### Simbolic shape & deduction
+在推导效率和shape信息量之间找到平衡：
+
+data-dependent shape : 插入match_cast，分配新符号，做runtime check。
+
+高效推导：每个pass跑完都会自动做shape inference。
+
+过程间shape关系：支持定义自定义子图或函数的输入输出shape关系，并支持caller能根据此作出正确的deduction。
+
+子图传入额外符号：例如输入输出都是表达式，relax选择将所需的符号全部作为参数传进去，而不是分配一个新符号表达子表达式替换之类。
+### fuse
+FuseTensorIR就是在merge call_tir
+### memory planning
+内存复用，看看RequestReuseWithSymShape，确认可以复用的严格条件。
+### Cross-Level Tensor Program Workspace Lifting
+###  CUDA Graph Offloading
+
+
 ## IR
 
 ## pass
--- build.fish
+++ build.fish
@@ -4,13 +4,14 @@ test -d build || mkdir build
 pushd build
   cp ../cmake/config.cmake .
   # controls default compilation flags (Candidates: Release, Debug, RelWithDebInfo)
-  echo "set(CMAKE_BUILD_TYPE RelWithDebInfo)" >> config.cmake
+  echo "set(CMAKE_BUILD_TYPE Debug)" >> config.cmake
 
   # LLVM is a must dependency for compiler end
   echo "set(USE_LLVM \"llvm-config --ignore-libllvm --link-static\")" >> config.cmake
   echo "set(HIDE_PRIVATE_SYMBOLS ON)" >> config.cmake
 
   # GPU SDKs, turn on if needed
+  echo "set(CMAKE_CUDA_ARCHITECTURES 80)" >> config.cmake
   echo "set(USE_CUDA   ON)" >> config.cmake
   echo "set(USE_METAL  OFF)" >> config.cmake
   echo "set(USE_VULKAN OFF)" >> config.cmake
@@ -19,10 +20,9 @@ pushd build
   # cuBLAS, cuDNN, cutlass support, turn on if needed
   echo "set(USE_CUBLAS ON)" >> config.cmake
   echo "set(USE_CUDNN  OFF)" >> config.cmake
-  echo "set(USE_CUTLASS OFF)" >> config.cmake
+  echo "set(USE_CUTLASS ON)" >> config.cmake
 
   set -a opts -GNinja
-  # set -a opts -DCUDA_CUDNN_LIBRARY=/usr/local/cuda/lib64 
 
   cmake $opts .. && cmake --build . 
 popd
